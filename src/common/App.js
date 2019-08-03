8e6c1c25079efb4179994f713937e02b332d397f
shiv pratap singh <pratapshiv1992@gmail.com>
2019-08-04 01:31:19 +0530

    dummy update post component created

--- src/common/App.js
+++ src/common/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import PostListing from '../components/PostListing';
 import CreatePost from '../components/CreatePost';
+import UpdatePost from '../components/UpdatePost';
 
 const Test = (props)=><>Hello Test</>
 const App = (props) => (
@@ -9,6 +10,7 @@ const App = (props) => (
     <Switch>
         <Route path='/' exact component={PostListing} />
         <Route path='/post/create' exact component={CreatePost} />
+        <Route path='/post/update/:id' exact component={UpdatePost} />
         <Redirect to="/" />
     </Switch>
     </>
---
+++ src/components/UpdatePost.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const UpdatePost = (props)=><h1>This is Update Post page...</h1>;
+
+export default UpdatePost;
