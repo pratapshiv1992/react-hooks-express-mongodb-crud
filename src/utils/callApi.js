b7f087132f460f671eaed99e8eb3a6b0f4b681f0
shiv pratap singh <pratapshiv1992@gmail.com>
2019-08-03 23:56:16 +0530

    client implemented to call the rest API

---
+++ src/utils/callApi.js
@@ -0,0 +1,23 @@
+const callAPi = ({url, method, data})=> {
+    fetch(url,{
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method,
+        body: JSON.stringify(data)
+    }).then(response =>
+        response.json().then(data => ({
+                data: data,
+                status: response.status
+            })
+        ).then(res => {
+            console.log(res.status, res.data)
+        })).catch(e => console.log('Oops! something went wrong',e));
+
+}
+
+module.exports ={
+    callAPi
+};
+
